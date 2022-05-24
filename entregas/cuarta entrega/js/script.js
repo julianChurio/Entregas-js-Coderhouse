0179493c73f58f8f5e06793cc51fee084f8ed9de
julianChurio <julichurio282@gmail.com>
2022-05-23 22:14:00 -0300

    agregue la cuarta entrega

---
+++ entregas/cuarta entrega/js/script.js
@@ -0,0 +1,17 @@
+function mostrarValoresDS1() {
+  let primerBotao = document.getElementById("primerButton");
+  primerBotao.className = "hidden";
+  document.getElementById("primerTexto").classList.remove("hidden");
+}
+
+function mostrarValoresDS2() {
+  let segundoBotao = document.getElementById("segundoButton");
+  segundoBotao.className = "hidden";
+  document.getElementById("segundoTexto").classList.remove("hidden");
+}
+
+function mostrarValoresDS3() {
+  let tercerBotao = document.getElementById("tercerButton");
+  tercerBotao.className = "hidden";
+  document.getElementById("tercerTexto").classList.remove("hidden");
+}
